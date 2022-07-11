b992203b00307f60a18b5d63f211bdd2be8c0a2a
Himanshu Goel <himanshu@hgoel.dev>
2022-07-11 14:05:15 -0400

    Increase gap between reports

--- code/run.sh
+++ code/run.sh
@@ -1,1 +1,1 @@
-python GAN_losses_iter.py --loss_D 4 --image_size 32 --seed 1 --lr_D .0002 --lr_G .0002 --batch_size 32 --Diters 1 --arch 0 --beta1 .50 --beta2 .999 --n_iter 100000 --gen_every 1000 --print_every 100 --G_h_size 64 --D_h_size 64 --gen_extra_images 10000 --CIFAR10 True
+python GAN_losses_iter.py --loss_D 4 --image_size 32 --seed 1 --lr_D .0002 --lr_G .0002 --batch_size 32 --Diters 1 --arch 0 --beta1 .50 --beta2 .999 --n_iter 100000 --gen_every 1000 --print_every 1000 --G_h_size 64 --D_h_size 64 --gen_extra_images 10000 --CIFAR10 True
